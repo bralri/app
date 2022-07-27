8d75c2833f354885a6ed01f8547ec53fc85fc389
Bryan Ridpath <bryanridpath@gmail.com>
2022-07-27 16:40:58 +0100

    favicon

--- build_a_vessel/vessel.js
+++ build_a_vessel/vessel.js
@@ -1,60 +1,66 @@
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    2000
-);
-camera.position.z = 4;
-
-const renderer = new THREE.WebGLRenderer({
-    alpha: true,
-    antialias: true 
-});
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-renderer.outputEncoding = THREE.sRGBEncoding;
-
-window.addEventListener('resize', function() {
-    const width = window.innerWidth;
-    const height = window.innerHeight;
-    renderer.setSize(width, height);
-    camera.aspect = width/height;
-    camera.updateProjectionMatrix();
-})
-
 let modelArray = [];
 
-const ambientLight = new THREE.AmbientLight(0xffffff);
-scene.add(ambientLight);
+let scene, camera, renderer, controls;
+let ambientLight;
+let loader;
+let width = window.innerWidth;
+let height = window.innerHeight;
 
-const loader = new THREE.GLTFLoader();
-for (let i = 0; i < 11; i++) {
+function init() {
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        2000
+    );
+    camera.position.z = 4;
 
-    loader.load(
-        '../../gltf/build_a_vessel_1/' + i + '.glb',
+    renderer = new THREE.WebGLRenderer({
+        alpha: true,
+        antialias: true 
+    });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+    renderer.outputEncoding = THREE.sRGBEncoding;
 
-        function(gltf) {
-            model = gltf.scene;
+    window.addEventListener('resize', function() {
+        renderer.setSize(width, height);
+        camera.aspect = width/height;
+        camera.updateProjectionMatrix();
+    })
 
-            model.position.x = (Math.random() - 0.5) * 8;
-            model.position.y = (Math.random() - 0.5) * 4;
+    ambientLight = new THREE.AmbientLight(0xffffff);
+    scene.add(ambientLight);
 
-            model.rotation.y = Math.random() * 2 * Math.PI;
-            
+    loader = new THREE.GLTFLoader();
+    for (let i = 0; i < 11; i++) {
 
-            scene.add(model);
-            modelArray.push(model);
-        }
-    )
+        loader.load(
+
+            '../../gltf/build_a_vessel_1/' + i + '.glb',
+
+            function(gltf) {
+                model = gltf.scene;
+
+                model.position.x = (Math.random() - 0.5) * 8;
+                model.position.y = (Math.random() - 0.5) * 4;
+
+                model.rotation.y = Math.random() * 4 * Math.PI;
+                
+                scene.add(model);
+                modelArray.push(model);
+            }
+        )
+    }
+
+    controls = new THREE.DragControls(modelArray, camera, renderer.domElement);
 }
-
-let controls = new THREE.DragControls(modelArray, camera, renderer.domElement);
 
 function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
 }
 
+init();
 animate();
