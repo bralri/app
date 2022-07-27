8cda35e148aa31645c26be132b2b54b22cc7158c
Bryan Ridpath <bryanridpath@gmail.com>
2022-07-27 18:06:59 +0100

    27/07/2022

--- build_a_vessel/vessel.js
+++ build_a_vessel/vessel.js
@@ -14,7 +14,7 @@ function init() {
         0.1,
         2000
     );
-    camera.position.z = 4;
+    camera.position.z = 4.5;
 
     renderer = new THREE.WebGLRenderer({
         alpha: true,
@@ -34,7 +34,7 @@ function init() {
     scene.add(ambientLight);
 
     loader = new THREE.GLTFLoader();
-    for (let i = 0; i < 13; i++) {
+    for (let i = 0; i < 15; i++) {
 
         loader.load(
 
@@ -43,9 +43,9 @@ function init() {
             function(gltf) {
                 model = gltf.scene;
 
-                model.position.x = (Math.random() - 0.5) * 7;
+                model.position.x = (Math.random() - 0.5) * 8.5;
                 model.position.y = (Math.random() - 0.5) * 3;
-                model.position.z = (Math.random() - 0.5) * 2;
+                model.position.z = (Math.random() - 0.5) * 1;
 
                 model.rotation.y = Math.random() * 4 * Math.PI;
                 
